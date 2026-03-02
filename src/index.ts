d54de6a23503dc290edfe1343bbd9c8bf758fb22
Joacy Oliveira <thesoldier100052@gmail.com>
2026-03-01 21:36:58 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
